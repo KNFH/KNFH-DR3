c0ead750b3f11c1cd3af5d1d040f0a4ca5dffa14
Hashem Samer <71471907+KNFH@users.noreply.github.com>
2021-05-20 05:24:38 +0300

    Upload

---
+++ server.lua
@@ -0,0 +1,11 @@
+QBCore = nil
+
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+QBCore.Functions.CreateUseableItem("shield", function(source, item)
+    --local identifiers = GetPlayerIdentifiers(src)
+    local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent("KNFH-DR3:CT:dr3", source)
+    -- [Normal Logs]
+    TriggerEvent("qb-log:server:CreateLog", "KNFH-DR3", "KNFH-Edition", "white", "اللاعب ارتدى درع السوات **"..GetPlayerName(source).."** Citizen ID : **"..Player.PlayerData.citizenid.. "**", false)
+end)
